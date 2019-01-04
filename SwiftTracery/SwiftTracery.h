a19999413e83c08b4e83b241f92a374495ac93ff
Neil Allain <njallain@bluerope.org>
2019-01-03 19:46:19 -0600

    Initial Commit

---
+++ SwiftTracery/SwiftTracery.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTracery.h
+//  SwiftTracery
+//
+//  Created by Neil Allain on 1/3/19.
+//  Copyright © 2019 Neil Allain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftTracery.
+FOUNDATION_EXPORT double SwiftTraceryVersionNumber;
+
+//! Project version string for SwiftTracery.
+FOUNDATION_EXPORT const unsigned char SwiftTraceryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTracery/PublicHeader.h>
+
+
